28d0f6d69c3766faf2fa9d78c9064592a49adfba
EdvaldoMelquiades <melquiades.emc@gmail.com>
2021-09-16 20:44:59 -0300

    Added project

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int valor = 100;
+    string texto;
+
+    void Start()
+    {
+        texto = valor.ToString ();
+
+        print (texto); // O resultado será 100, só que em string
+    }
+
+    void Update()
+    {
+        
+    }
+}
